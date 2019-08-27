67f70314882262f12430ef9c4d260ac78116f60f
xunjin <xunjin@localhost.localdomain>
2019-08-27 14:17:48 -0300

    Stabilizing the master branch

--- bothello.py
+++ bothello.py
@@ -10,9 +10,6 @@ token_bot = environ.get('BOT')
 client = discord.Client()
 
 
-client.add_cog(Cog(client))
-
-
 @client.event
 async def on_ready():
     print(f'We have looged in as {client.user}')
